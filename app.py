a25a7e41f9bf4a6042822711cbdbc60548b44785
ArifPeycal <ariffaisal021123@gmail.com>
2024-06-03 22:47:31 +0800

    Update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.preprocessing import image
+from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.utils import load_img
 from efficientnet.tfkeras import EfficientNetB0
 import numpy as np
 app = Flask(__name__)
@@ -22,8 +24,8 @@ meso.make_predict_function()
 # 	return dic[int(x)], y
 
 def predict_deepfake(img_path):
-	i = image.load_img(img_path, target_size=(256,256))
-	i = image.img_to_array(i)/255.0
+	i = load_img(img_path, target_size=(256,256))
+	i = img_to_array(i)/255.0
 	i = i.reshape(1,256,256,3)
 	preds = model.predict(i)
 	x = (preds > 0.5).astype("int32")
@@ -33,7 +35,19 @@ def predict_deepfake(img_path):
 # routes
 @app.route("/", methods=['GET', 'POST'])
 def main():
-	return render_template("index.html")
+	return render_template("main.html")
+
+@app.route("/models", methods=['GET', 'POST'])
+def models():
+	return render_template("models.html")
+
+@app.route("/image", methods=['GET', 'POST'])
+def image():
+	return render_template("image.html")
+
+@app.route("/drag", methods=['GET', 'POST'])
+def drag():
+	return render_template("drag.html")
 
 @app.route("/about")
 def about_page():
@@ -44,7 +58,7 @@ def get_output():
 	if request.method == 'POST':
 		img = request.files['my_image']
 
-		img_path = "static/" + img.filename	
+		img_path = "static/images/" + img.filename	
 		img.save(img_path)
 
 		# p, confidence = predict_label(img_path)
@@ -56,7 +70,7 @@ def get_output():
 		if (confidences < 0.5):
 			confidences = 1 - confidences
 		confidences = "{:.02%}".format(confidences)
-	return render_template("index.html",  img_path = img_path, deepfake = d, confidences = confidences)
+	return render_template("main.html",  img_path = img_path, deepfake = d, confidences = confidences)
 
 
 if __name__ =='__main__':
